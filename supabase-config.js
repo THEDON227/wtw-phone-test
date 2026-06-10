0b3c4a7bed5adced109b26422bde9847751fc46d
THEDON227 <eshunkwaym@gmail.com>
2026-06-10 15:45:06 +0000

    fix: add public Supabase config for GitHub Pages

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.WTW_SUPABASE_CONFIG = window.WTW_SUPABASE_CONFIG || {
+  url: 'https://qtvpygytqcjbmhljsvui.supabase.co',
+  anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0dnB5Z3l0cWNqYm1obGpzdnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NzIzMzIsImV4cCI6MjA5NjM0ODMzMn0.HIGqJgQKUBKJlngWzmLMFO_0fELqsqXcnEOWPwCCLYE'
+};
